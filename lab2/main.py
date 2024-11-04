e3872db6be808dafe9c691a84049ec303155f01a
eikatkoffbmstu <147157589+eikatkoffbmstu@users.noreply.github.com>
2024-11-04 17:57:24 +0300

    adding initial conditions main.py

--- lab2/main.py
+++ lab2/main.py
@@ -1,1 +1,20 @@
 
+import simplex
+
+
+c = np.array([5, 6, 1])
+A = np.array([
+    [2, 1, 1],
+    [1, 2, 0],
+    [0, 0.5, 1]
+])
+b = np.array([5, 3, 8])
+
+c_dual = b
+A_dual = -A.T
+b_dual =-c
+simplex_dual = SimplexMethod(c_dual, A_dual, b_dual, "min")
+if simplex_dual.solution():
+    simplex_dual.print_table()
+    simplex_dual.print_solution()
+    simplex_dual.check_solution()
